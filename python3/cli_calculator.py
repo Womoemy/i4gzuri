d0992be9a304aa352ac4c9ffa36b1d5959e0143f
Womo Emmanuel Ebiobowei <womoemiebiobowei@gmail.com>
2022-05-21 14:15:09 +0100

    rewrite program with a while loop and create validation

--- python3/cli_calculator.py
+++ python3/cli_calculator.py
@@ -1,23 +1,38 @@
-# A python program that handles simple calculation
+# A simple calculator in Python
+print("SIMPLE CLI CALCULATOR")
 
-operation = input('''
-SIMPLE CLI CALCULATOR
-***********************************************
-Choose an operation you want to perform:
-Pick any of these ['+', '-', 'x', '/', '%']
+is_started = True
 
-''')
+while is_started:
 
-num_1 = int(input('First number: '))
-num_2 = int(input('Second number: '))
+  operation = input("What operation would you want to perform?\nPick any of these ['+', '-', 'x', '/', '%'] : ")
 
-if operation == "+":
-  print('{} + {} = '.format(num_1, num_2) + str(num_1 + num_2))
-elif operation == "-":
-  print('{} - {} = '.format(num_1, num_2) + str(num_1 - num_2))
-elif operation == "x":
-  print('{} x {} = '.format(num_1, num_2) + str(num_1 * num_2))
-elif operation == "/":
-  print('{} / {} = '.format(num_1, num_2) + str(num_1 / num_2))
-else:
-  print('{} % {} = '.format(num_1, num_2) + str(num_1 % num_2))
+  try: # Try to get user input
+    num_1 = int(input('First number: '))
+    num_2 = int(input('Second number: '))
+  except: # if invalid return an error
+    print("Failed, enter a valid number...")
+    continue
+
+  # Calculator main logic 
+  if operation == "+":
+    print('{} + {} = '.format(num_1, num_2) + str(num_1 + num_2))
+  elif operation == "-":
+    print('{} - {} = '.format(num_1, num_2) + str(num_1 - num_2))
+  elif operation == "x":
+    print('{} x {} = '.format(num_1, num_2) + str(num_1 * num_2))
+  elif operation == "/":
+    print('{} / {} = '.format(num_1, num_2) + str(num_1 / num_2))
+  elif operation == "%":
+    print('{} % {} = '.format(num_1, num_2) + str(num_1 % num_2))
+  else:
+    print("Invalid operation entered, try again...")
+
+  # For multiple calculations
+  choice = input("Would you like to run another calculation? [y,n]: ")
+  if choice.upper() == "Y":
+    pass
+  if choice.upper() == "N":
+    break # checking to see if this is the same thing as setting is_started = False
+
+print("Thank for using the Calculator. See you later!")
