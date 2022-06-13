3def0504de5bdb787103354a0dc61a8d0988a371
Womo Emmanuel Ebiobowei <womoemiebiobowei@gmail.com>
2022-06-13 19:33:06 +0100

    create main.py and initialize readme

---
+++ python3/rps/main.py
@@ -0,0 +1,13 @@
+# Rock, Paper, Scissors Game
+print("/*====================================*/")
+print("/*      ROCK, PAPER, SCISSORS         */")
+print("/*====================================*/")
+
+play_game = True
+
+while play_game:
+  def playMatch():
+    playerChoice = input("R [Rock], P [Paper] or S [Scissors]: ")
+
+# Exit the game
+print("The winner will be declared soon! See you soon!")
