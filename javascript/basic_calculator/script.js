9d24c27f1ad6c50eda171092782c6cd12ec2be36
Womo Emmanuel Ebiobowei <womoemiebiobowei@gmail.com>
2022-06-24 21:54:43 +0100

    update README file

---
+++ javascript/basic_calculator/script.js
@@ -0,0 +1,4 @@
+let operator = prompt("What operation do you want to perform\n['+', '-', '/' or 'x']: ");
+let num1 = prompt("Enter first number: ", "0");
+let num2 = prompt("Enter second number: ", "0");
+num1 = parseInt
