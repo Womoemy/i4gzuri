fb798ba88bfbf44442482e473359b0bac76d8363
Womo Emmanuel Ebiobowei <womoemiebiobowei@gmail.com>
2022-06-26 22:22:57 +0100

    complete javascript basic arithmetic calculator

--- javascript/basic_calculator/script.js
+++ javascript/basic_calculator/script.js
@@ -1,4 +1,57 @@
-let operator = prompt("What operation do you want to perform? \n[' + ', ' - ', ' / ', ' x ']: ");
-let num1 = prompt("Enter first number: ", "0");
-let num2 = prompt("Enter second number: ", "0");
-num1 = parseInt
+let operator = prompt("What operation do you want to perform? [' + ', ' - ', ' / ', ' x ']: ");
+let num1 = parseInt(prompt("Enter first number: ", "0"));
+let num2 = parseInt(prompt("Enter second number: ", "0"));
+let result;
+
+switch(operator) {
+  case "+":
+    result = `${num1} + ${num2} = ${num1 + num2}`;
+    alert(result);
+    break;
+  case "-":
+    result = `${num1} - ${num2} = ${num1 - num2}`;
+    alert(result);
+    break;
+  case "x":
+    result = `${num1} * ${num2} = ${num1 * num2}`;
+    alert(result);
+    break;
+  case "*":
+    result = `${num1} * ${num2} = ${num1 * num2}`;
+    alert(result);
+    break;
+  case "/":
+    result = `${num1} * ${num2} = ${num1 * num2}`;
+    alert(result);
+    break;
+  default:
+    alert("Invalid operation entered, try again...");
+}
+// Exit the program
+// alert("Thank for using the Calculator. See you later!");
+
+// let operator = prompt("What operation do you want to perform? [' + ', ' - ', ' / ', ' x ']: ");
+// let num1 = parseInt(prompt("Enter first number: ", "0"));
+// let num2 = parseInt(prompt("Enter second number: ", "0"));
+// let result;
+
+// switch(operator) {
+//   case "+":
+//     result = `${num1} + ${num2} = ${num1 + num2}`;
+//     alert(result);
+//     break;
+//   case "-":
+//     result = `${num1} - ${num2} = ${num1 - num2}`;
+//     alert(result);
+//     break;
+//   case "x":
+//     result = `${num1} * ${num2} = ${num1 * num2}`;
+//     alert(result);
+//     break;
+//   case "/":
+//     result = `${num1} * ${num2} = ${num1 * num2}`;
+//     alert(result);
+//     break;
+//   default:
+//     alert("Invalid operation entered, try again...");
+// }
